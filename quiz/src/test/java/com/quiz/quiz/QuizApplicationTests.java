1d65dab50b8c41d32ae0346678ad2410bfb6e44d
achrafmz <achrafmazouz50@gmail.com>
2025-05-23 19:49:47 +0100

    nouveau commit

--- quiz/src/test/java/com/quiz/quiz/QuizApplicationTests.java
+++ quiz/src/test/java/com/quiz/quiz/QuizApplicationTests.java
@@ -1,13 +1,29 @@
-	package com.quiz.quiz;
+package com.quiz.quiz;
 
-	import org.junit.jupiter.api.Test;
-	import org.springframework.boot.test.context.SpringBootTest;
+import com.quiz.quiz.Config.FirebaseConfig;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.ContextConfiguration;
 
-	@SpringBootTest
-	class QuizApplicationTests {
+@SpringBootTest
+@ContextConfiguration(classes = {QuizApplicationTests.TestConfig.class})
+public class QuizApplicationTests {
 
-		@Test
-		void contextLoads() {
+	@Configuration
+	static class TestConfig {
+		@Bean
+		@Primary
+		public FirebaseConfig firebaseConfig() {
+			return org.mockito.Mockito.mock(FirebaseConfig.class);
 		}
-
 	}
+
+	@Test
+	void contextLoads() {
+		// Test du chargement du contexte
+	}
+}
