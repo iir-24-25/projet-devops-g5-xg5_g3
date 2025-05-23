20f6be16bca4dce66d4b36246344813b099b9ba3
achrafmz <achrafmazouz50@gmail.com>
2025-05-23 20:49:22 +0100

    modification du fichier app.test.js

--- app-plats-frontend/src/App.test.js
+++ app-plats-frontend/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Accueil component', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  // Remplace ce texte par un contenu réel de la page Accueil.js
+  const accueilElement = screen.getByText(/Accueil/i);
+  expect(accueilElement).toBeInTheDocument();
 });
