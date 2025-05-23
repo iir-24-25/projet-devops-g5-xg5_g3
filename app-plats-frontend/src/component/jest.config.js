86dbf24595f1e574a7e4c3e508e24b19b7a40326
achrafmz <achrafmazouz50@gmail.com>
2025-05-23 21:17:38 +0100

    ajout du fichier jest.config.js

---
+++ app-plats-frontend/src/component/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^react-router-dom$': '<rootDir>/node_modules/react-router-dom',
+  },
+};
